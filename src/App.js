0b69766a79ac6e6760cb1dc043be976e97c45872
ktr11 <kenmcz315@gmail.com>
2020-11-21 16:18:09 +0900

    routingを設定

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
 
 export default function App() {
-  render() {
-    return (
-      <div>
-
-      </div>
-    )
-  }
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route exact path={"/dashboard"} component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
