7b64c18569b4a6cda6ac52655cc818d35e65a760
ktr11 <kenmcz315@gmail.com>
2020-11-22 22:42:11 +0900

    ユーザー登録機能　実装

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,33 +1,56 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function Registration() {
-    // useState()を用いて、ユーザーデータの初期値（空の文字列）を定義する。
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [passwordConfirmation, setPasswordConfirmation] = useState("")
 
+    const handleSubmit = (event) => {
+        axios.post("http://localhost:3001/signup",
+            {
+                user: {
+                    email: email,
+                    password: password,
+                    password_confirmation: passwordConfirmation
+                }
+            },
+            { withCredentials: true }
+        ).then(response => {
+            console.log("registration res", response)
+        }).catch(error => {
+            console.log("registration error", error)
+        })
+        event.preventDefault()
+    }
+
     return (
         <div>
            <p>新規登録</p>
-            <form>
+
+            <form onSubmit={handleSubmit}>
                 <input
                     type="email"
                     name="email"
                     placeholder="メールアドレス"
                     value={email}
+                    onChange={event => setEmail(event.target.value)}
                 />
                 <input
                     type="password"
                     name="password"
                     placeholder="パスワード"
                     value={password}
+                    onChange={event => setPassword(event.target.value)}
                 />
                 <input
                     type="password"
                     name="password_confirmation"
                     placeholder="確認用パスワード"
                     value={passwordConfirmation}
+                    onChange={event => setPasswordConfirmation(event.target.value)}
                 />
+
                 <button type="submit">登録</button>
             </form>
         </div>
