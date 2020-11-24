6f727df1747564f57d501a01f7eee448c711973b
ktr11 <kenmcz315@gmail.com>
2020-11-24 09:23:11 +0900

    ログイン処理追加

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
 
 export default function App() {
+  const [loggedInStatus, setLoggedInStatus] = useState("未ログイン")
+  const [user, setUser] = useState({})
+  const handleLogin = (data) => {
+    setLoggedInStatus("ログインなう")
+    setUser(data.user)
+  }
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path={"/"} component={Home} />
-          <Route exact path={"/dashboard"} component={Dashboard} />
+          <Route
+            exact path={"/"}
+            render={props => (
+              <Home {...props} handleLogin={handleLogin} loggedInStatus={loggedInStatus} />
+            )}
+          />
+          <Route
+            exact path={"/dashboard"}
+            render={props => (
+              <Dashboard {...props} loggedInStatus={loggedInStatus} />
+            )}
+          />
         </Switch>
       </BrowserRouter>
     </div>
